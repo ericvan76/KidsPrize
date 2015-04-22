9aad0f00861517060e8b79c18ff4abbf23edcda6
Eric Fan <ericvan76@gmail.com>
2015-04-23 08:50:43 +0930

    Added oauth support and restructured the folders

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 'use strict';
 
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var child = require('./routes/child');
-
-var app = express();
+var express = require('express'),
+  app = module.exports = express(),
+  path = require('path'),
+  favicon = require('serve-favicon'),
+  logger = require('morgan'),
+  cookieParser = require('cookie-parser'),
+  bodyParser = require('body-parser'),
+  session = require('express-session'),
+  passport = require('./lib/auth/passport'),
+  home = require('./lib/home'),
+  auth = require('./lib/auth'),
+  user = require('./lib/user'),
+  config = require('./lib/config');
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'lib/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(config.session));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/child', child);
+// initialise passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// plug your sub apps here
+app.use(home);
+app.use('/auth', auth);
+app.use('/user', user);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -39,27 +47,16 @@ app.use(function(req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  var stacktrace = {};
+  if (process.env.NODE_ENV === 'development') {
+    // will print stack trace to user
+    stacktrace = err;
+  }
   res.status(err.status || 500);
   res.render('error', {
+    title: 'Error',
     message: err.message,
-    error: {}
+    error: stacktrace
   });
 });
-
-module.exports = app;
---
+++ server.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var cluster = require('cluster');
+var debug = require('debug')('kids-reward:server');
+
+if (cluster.isMaster) {
+  // In real life, you'd probably use more than just 2 workers,
+  // and perhaps not put the master and worker in the same file.
+  cluster.fork();
+  // cluster.fork();
+
+  cluster.on('disconnect', function(worker) {
+    console.error('disconnect!');
+    cluster.fork();
+  });
+
+} else {
+  // the worker
+  var config = require('./lib/config'),
+    port = normalizePort(process.env.PORT || '3000');
+
+  // create domain
+  var domain = require('domain').create();
+  var bind = typeof port === 'string' ? 'pipe ' + port : 'port ' + port;
+  domain.on('error', function(err) {
+    switch (err.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw err;
+    }
+  });
+
+  domain.run(function() {
+
+    var mongoose = require('mongoose'),
+      http = require('http'),
+      app = require('./app');
+
+    // connect mongodb
+    mongoose.connect(config.mongoose.dbURL, config.mongoose.options || {});
+    mongoose.set('debug', config.mongoose.debug || false);
+
+    // create server
+    var server = http.createServer(app);
+    server.on('listening', function() {
+      var addr = server.address();
+      var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+      console.log('Listening on ' + bind);
+    });
+    app.set('port', port);
+    // listen
+    server.listen(port);
+  });
+}
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+  return false;
+}
