f153d95adb94ce1d7fb59530f6960c4a41582d51
Eric Fan <eric.fan@alcidion.com>
2017-03-21 09:57:11 +1030

    Fix travis config

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -10,21 +10,20 @@ dotnet publish -c Release ./KidsPrize.Http/KidsPrize.Http.csproj -o $(pwd)/publi
 # Determine $TAG by GIT BRANCH AND TAG
 echo TRAVIS_BRANCH=$TRAVIS_BRANCH
 echo TRAVIS_TAG=$TRAVIS_TAG
-if [ "$TRAVIS_BRANCH" = "master" ]; then export TAG=latest; fi
-if [ "$TRAVIS_BRANCH" = "develop" ]; then export TAG=develop; fi
-if [ "$TRAVIS_TAG" != "" ]; then export TAG=$TRAVIS_TAG; fi
+if [[ $TRAVIS_BRANCH = master ]]; then export TAG=latest; fi
+if [[ ! -z $TRAVIS_TAG ]]; then export TAG="$TRAVIS_TAG"; fi
 
 # Validate $TAG
 echo TAG=$TAG
-if [ "$TAG" = "" ]; then exit 1; fi
+if [[ -z $TAG ]]; then exit 1; fi
 export IMAGE_TAG="ericvan76/kidsprize:$TAG"
-echo IMAGE_TAG=$IMAGE_TAG
+echo IMAGE_TAG="$IMAGE_TAG"
 
 # build docker
 docker build publish -t "$IMAGE_TAG"
 
 # do not push docker images for pull request
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then exit 0; fi
+if [[ $TRAVIS_PULL_REQUEST = true ]]; then exit 0; fi
 
 # docker push
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
