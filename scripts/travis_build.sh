234be01eaf35bf582fea2a52d74269ae6c927982
Eric Fan <ericvan76@gmail.com>
2017-08-23 22:08:12 +0930

    Update docker build

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -7,25 +7,20 @@ dotnet test ./KidsPrize.Tests/KidsPrize.Tests.csproj
 rm -rf $(pwd)/publish
 dotnet publish -c Release ./KidsPrize.Http/KidsPrize.Http.csproj -o $(pwd)/publish
 
-# Determine $TAG by GIT BRANCH AND TAG
-echo TRAVIS_BRANCH=$TRAVIS_BRANCH
-echo TRAVIS_TAG=$TRAVIS_TAG
-if [[ $TRAVIS_BRANCH = master ]]; then export TAG=latest; fi
-if [[ ! -z $TRAVIS_TAG ]]; then export TAG="$TRAVIS_TAG"; fi
+# do not build docker for pull request
+if [[ $TRAVIS_PULL_REQUEST = true ]]; then exit 0; fi
 
-# Validate $TAG
-echo TAG=$TAG
-if [[ -z $TAG ]]; then exit 1; fi
-export IMAGE_TAG="ericvan76/kidsprize:$TAG"
-echo IMAGE_TAG="$IMAGE_TAG"
+export REPO_NAME="ericvan76/kidsprize"
 
 # build docker
-docker build publish -t "$IMAGE_TAG"
+docker build publish -t $REPO_NAME
 
-# do not push docker images for pull request
-if [[ $TRAVIS_PULL_REQUEST = true ]]; then exit 0; fi
+# tag
+if [[ ! -z $TRAVIS_TAG ]]
+then
+    docker tag $REPO_NAME $REPO_NAME:$TRAVIS_TAG
+fi
 
 # docker push
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push "$IMAGE_TAG"
-
+docker push $REPO_NAME
